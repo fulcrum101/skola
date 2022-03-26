e7149a34e8eb5183d5c9871fc436a9726cfcaedd
mmuffin101 <69482465+mmuffin101@users.noreply.github.com>
2022-03-26 13:30:08 +0200

    lol

---
+++ script.js
@@ -0,0 +1,23 @@
+function main()
+{
+    window.location.href = "index.html";
+}
+
+function Vesture()
+{
+    window.location.href = "article_1_text.html";
+}
+
+function Nosaukumi()
+{
+    window.location.href = "article_2_text.html";
+}
+
+function Vaditaji()
+{
+    window.location.href = "article_3_text.html";
+}
+function Absolventi()
+{
+    window.location.href = "article_4_text.html";
+}
